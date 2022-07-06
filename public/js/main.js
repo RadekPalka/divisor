900c54c6d504bba5b6e86ee61e3078d4ce29f706
Radoslaw Palka <radekpalka@gmail..com>
2022-07-06 20:58:50 +0200

    fetch data to backend

--- public/js/main.js
+++ public/js/main.js
@@ -2,18 +2,29 @@ const firstNumberInput = document.querySelector("#first-number");
 const secondNumberInput= document.querySelector("#second-number");
 const form = document.querySelector("form");
 
-const submitForm = e =>{
+const submitForm = async e=>{
 
     e.preventDefault();
 
     if (!firstNumberInput.value || !secondNumberInput.value){
-        alert("Musisz wypełnić formularz");
+        return alert("Musisz wypełnić formularz");
     }
     const firstNumber = +firstNumberInput.value;
     const secondNumber = +secondNumberInput.value;
     if (secondNumber >= firstNumber || secondNumber <3 || firstNumber < 3){
-        alert("Weż się postaraj")
+        return alert("Weż się postaraj");
     }
+    const result = await fetch("/calc/check", {
+        method: 'POST',
+        body: JSON.stringify({
+            firstNumber,
+            secondNumber,
+        }),
+        headers: {
+            "Content-Type": 'application/json',
+        }
+            })
+    const data= await result.json();
 }
 
 form.addEventListener("submit", e => submitForm(e));
