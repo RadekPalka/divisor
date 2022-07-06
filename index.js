aa4858553d4e5c89e47e55db6cc5b143e1150e2c
Radoslaw Palka <radekpalka@gmail..com>
2022-07-06 20:02:21 +0200

    add index.html and create form

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express()
 
-app.get('/', (req, res)=>{
-    res.send("<h1>It works</h1>")
-})
+app.use(express.static("public"))
+
+
 
 app.listen(3000)
